4433085781e08311d6f1372d34770b695afbc997
Andarilz <tartarus@gmail.com>
2022-02-19 04:20:51 +0300

    'routes'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {AuthComponent} from "./auth/auth.component";
+import {RegComponent} from "./reg/reg.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+      {path: '', component: HomeComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'reg', component: RegComponent},
+      {path: 'auth', component: AuthComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
